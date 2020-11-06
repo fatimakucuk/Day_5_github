535e6c353c377ba87be73f9aa411bff9db101cef
fatimakucuk <72092041+fatimakucuk@users.noreply.github.com>
2020-11-06 19:17:43 +0100

    Update Helloo.java
    GITHUB dan ekleme denemesi

--- src/test/java/Helloo.java
+++ src/test/java/Helloo.java
@@ -4,6 +4,7 @@ public class Helloo {
     public static void main(String[] args) {
         System.out.println("hellooooo new helloooooooooooo"  FROM GITHUB SOME CAHANGES);
         
+        //GITHUB DAN EKLEME YAPTIM
         
     }
 }
