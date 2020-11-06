23b20caed19958b99904b4a597273bc02626bf62
fatimakucuk <72092041+fatimakucuk@users.noreply.github.com>
2020-11-06 19:29:26 +0100

    co-woker

--- src/test/java/Helloo.java
+++ src/test/java/Helloo.java
@@ -2,7 +2,10 @@ public class Helloo {
 
 
     public static void main(String[] args) {
-        System.out.println("hellooooo new helloooooooooooo");
+        System.out.println("hellooooo new helloooooooooooo"); 
         System.out.println("feature2");
+        
+        
+        //GITHUB DAN EKLEME YAPTIM            co-woker co woker
     }
 }
