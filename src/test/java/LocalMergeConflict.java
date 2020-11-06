b011b90a7065fe6499446ca91a1774b3a5bf435a
FATIMAKUCUK <fatimakucuk35@gmail.com>
2020-11-06 20:08:07 +0100

    hdjfkopkl`pñ fgh4

--- src/test/java/LocalMergeConflict.java
+++ src/test/java/LocalMergeConflict.java
@@ -4,6 +4,7 @@ public class LocalMergeConflict {
     public static void main(String[] args) {
         System.out.println("added some line im local master branch");
         System.out.println("added");
+        //fghjklkjijhg
 
     }
 }
